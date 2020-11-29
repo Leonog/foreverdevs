adc82448dd93658497ec46be60bfd24ebb0d0089
Leonardo Nogueira <leonardonogu037@gmail.com>
2020-11-28 23:03:35 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          foreverdevs - under construction
+          foreverdevs - under construction ...
         </p>
       </header>
     </div>
